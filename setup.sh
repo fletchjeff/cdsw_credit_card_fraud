5f82ccf15bb810b19d828fd8fd38bba590d3a26d
Jeff Fletcher <jeff@splongebook.local>
2019-04-16 12:30:39 +0200

    Typo

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 # this will unzip and copy the .csv file into your HDFS home directory
-unzip resources/creditcard.csz.zip
+unzip resources/creditcard.csv.zip
 hdfs dfs -copyFromLocal resources/creditcard.csv creditcard.csv
